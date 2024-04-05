af6c65ee69814bd992b07f169326663edda2b1f4
Aleksandra <25868183+volkova229@users.noreply.github.com>
2024-04-05 18:32:22 +0200

    Change colors

--- repository/src/main/java/UniqueEffort.java
+++ repository/src/main/java/UniqueEffort.java
@@ -17,7 +17,7 @@ public class UniqueEffort extends Robot
 		// After trying out your robot, try uncommenting the import at the top,
 		// and the next line:
 
-		setColors(Color.red, Color.blue, Color.green); // body,gun,radar
+		setColors(Color.magenta, Color.gray, Color.pink); // body,gun,radar
 
 		// Robot main loop
 		while(true) {
